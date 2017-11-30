5ed4fe50f88c66ef2149617075fda3b8b0d91a8c
Kurooka Toshiyuki <kurooka@webtech.co.jp>
2017-11-30 14:40:14 +0900

    Juce CachedValue< Type >の検証コード

---
+++ CachedValueTest/Source/Main.cpp
@@ -0,0 +1,54 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+//==============================================================================
+int main (int argc, char* argv[])
+{
+	juce::UndoManager undomanager(200, 30);
+
+	juce::ValueTree v(juce::Identifier("vertices"));
+
+	juce::CachedValue<int> ch_int( v,"int", &undomanager);
+	juce::CachedValue<float> ch_float(v, "int", &undomanager);
+	juce::CachedValue<double> ch_double(v, "double", &undomanager);
+
+	undomanager.beginNewTransaction();
+	ch_int.setDefault(10);
+
+	undomanager.beginNewTransaction();
+	ch_int.setValue(5, &undomanager);
+
+	juce::Logger::outputDebugString(juce::String(ch_int));		//-> 5
+
+	juce::Logger::outputDebugString(juce::String(ch_int.getDefault())); // ->10
+
+	//デフォルト値に戻す
+	ch_int.resetToDefault();
+	juce::Logger::outputDebugString(juce::String(ch_int)); //->10
+
+	//undo
+	undomanager.undo();
+	juce::Logger::outputDebugString(juce::String(ch_int)); //->10
+
+	undomanager.beginNewTransaction();
+	ch_int.setValue(5, &undomanager);
+
+	undomanager.beginNewTransaction();
+	ch_int.setValue(12, &undomanager);
+
+	undomanager.undo();
+	juce::Logger::outputDebugString(juce::String(ch_int)); //->5
+
+
+    return 0;
+}
